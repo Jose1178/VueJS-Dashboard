23b7d4a55023bf9281197bf5af8821e891eae8d6
Jose1178 <wamiti8711@gmail.com>
2023-10-17 16:21:34 +0300

    Feat: Routing

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-content>
+      <router-view></router-view>
+    </v-content>   
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  data () {
+    return {
+      
+    }
+  }
 }
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home-page">
+        <h1>Homepage</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
