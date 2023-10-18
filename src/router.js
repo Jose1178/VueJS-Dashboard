e379feb6ee8cd753e312aa212cff0b0d91f4ef1c
Jose1178 <wamiti8711@gmail.com>
2023-10-18 11:12:25 +0300

    Feat: Dashboard

--- src/router.js
+++ src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+import Dashboard from './views/Dashboard.vue'
+import Projects from './views/Projects.vue'
+import Team from './views/Team.vue'
+
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
+    {
+        path: '/',
+        name: 'dashboard',
+        component: Dashboard
+      },
+      {
+        path: '/projects',
+        name: 'projects',
+        component: Projects
+      },
+      {
+        path: '/team',
+        name: 'team',
+        component: Team
+      }
 ];
 
 const router = createRouter({
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Dashboard</h1>
+
+    <v-container class="my-5">
+
+      <v-layout row justify-start class="mb-3">
+        <v-tooltip top>
+          <template v-slot:activator>
+            <v-btn small flat color="grey" @click="sortBy('title')">
+              <v-icon small left>folder</v-icon>
+              <span class="caption text-lowercase">By project name</span>
+            </v-btn>
+          </template>
+          <span>Sort by project name</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <template v-slot:activator>
+            <v-btn small flat color="grey" @click="sortBy('person')">
+              <v-icon small left>person</v-icon>
+              <span class="caption text-lowercase">By Person</span>
+            </v-btn>
+          </template>
+          <span>Sort by project author</span>
+        </v-tooltip>
+      </v-layout>
+
+      <!-- Rest of your code -->
+
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardView', // Change the component name
+  data() {
+    return {
+      projects: [
+        // Your project data
+      ]
+    }
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+/* Your styles */
+
+</style>
